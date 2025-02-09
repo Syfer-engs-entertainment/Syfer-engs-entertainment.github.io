aaf8cedc6539579b2664624b684afc786d8a99ae
Syfer-eng <reviewedpuppy93@gmail.com>
2025-02-08 22:52:13 -0600

    Create config.js

---
+++ School-Github/Github/config.js
@@ -0,0 +1,10 @@
+const config = {
+    apiBaseUrl: 'https://api.github.com',
+    token: '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',
+    headers: {
+        'Accept': 'application/vnd.github.v3+json',
+        'Authorization': 'token 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'
+    },
+    defaultBranch: 'main',
+    perPage: 30
+};
